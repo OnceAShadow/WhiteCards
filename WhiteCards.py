6fee162186acdaafe8d5c8100290cfe1797015b4
Fred Sevillano <fred.sevillano@gmail.com>
2024-04-01 23:36:51 +0200

    justify, padding and wrap fixed

--- WhiteCards.py
+++ WhiteCards.py
@@ -4,45 +4,42 @@ from tkinter import *
 root = Tk()
 root.title("White Cards")
 root.configure(background="grey")
-root.geometry("1000x600")
-root.maxsize(1000, 600)
+root.geometry("1200x700")
+root.maxsize(1200, 700)
 
-Question = Label(root, text="Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", wraplength=920)
-Question.pack(side="top", anchor="nw", padx=40, pady=20)
+Question = Label(root, text="Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", wraplength=1120, justify=LEFT)
+Question.pack(side="top", anchor="nw", padx=40, pady=25)
 Question.config(font=("Font", 18))
-# wrap text
-# change color
 
 statusA = IntVar()
-answerA = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusA, wraplength=860)
-answerA.pack(side="top", anchor="nw", padx=60, pady=7)
-answerA.config(font=("Font", 14))
+answerA = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusA, wraplength=1000, justify=LEFT)
+answerA.pack(side="top", anchor="nw", padx=70, pady=8)
+answerA.config(font=("Font", 15))
 
 statusB = IntVar()
-answerB = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks", variable=statusB, wraplength=860)
-answerB.pack(side="top", anchor="nw", padx=60, pady=7)
-answerB.config(font=("Font", 14))
+answerB = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks", variable=statusB, wraplength=1000, justify=LEFT)
+answerB.pack(side="top", anchor="nw", padx=70, pady=8)
+answerB.config(font=("Font", 15))
 
 statusC = IntVar()
-answerC = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusC, wraplength=860)
-answerC.pack(side="top", anchor="nw", padx=60, pady=7)
-answerC.config(font=("Font", 14))
+answerC = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusC, wraplength=1000, justify=LEFT)
+answerC.pack(side="top", anchor="nw", padx=70, pady=8)
+answerC.config(font=("Font", 15))
 
 statusD = IntVar()
-answerD = Checkbutton(root, text="thisskjdhsakdfhdfkdsfhksdfhskjfhskjdfhsdkjfhsdkjfhsdkjfhsdkjfhsdkjh is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusD, wraplength=860)
-answerD.pack(side="top", anchor="nw", padx=60, pady=7)
-answerD.config(font=("Font", 14))
+answerD = Checkbutton(root, text="thisskjdhsakdfhdfkdsfhksdfhskjfhskjdfhsdkjfhsdkjfhsdkjfhsdkjfhsdkjh is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusD, wraplength=1000, justify=LEFT)
+answerD.pack(side="top", anchor="nw", padx=70, pady=8)
+answerD.config(font=("Font", 15))
 
 statusE = IntVar()
-answerE = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusE, wraplength=860)
-answerE.pack(side="top", anchor="nw", padx=60, pady=7)
-answerE.config(font=("Font", 14))
+answerE = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusE, wraplength=1000, justify=LEFT)
+answerE.pack(side="top", anchor="nw", padx=70, pady=8)
+answerE.config(font=("Font", 15))
 
 statusF = IntVar()
-answerF = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusF, wraplength=860)
-answerF.pack(side="top", anchor="nw", padx=60, pady=7)
-answerF.config(font=("Font", 14))
-
+answerF = Checkbutton(root, text="this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant questionAsk a Question? Ask a Question? this is a very very vyer aflajlkgjalk long question that I need to ask to check what it looks like when there is a giant question", variable=statusF, wraplength=1000, justify=LEFT)
+answerF.pack(side="top", anchor="nw", padx=70, pady=8)
+answerF.config(font=("Font", 15))
 
 def submit():
     print("Answer Submited")
@@ -54,9 +51,4 @@ Count.config(font=("Font", 14))
 submit_button = Button(root, text="Done", height=2, width=5, command=submit)
 submit_button.pack(side="bottom", anchor="se", padx=30)
 
-
-
-
-
 root.mainloop()
-
